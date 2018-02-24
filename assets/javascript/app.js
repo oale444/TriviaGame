abfe9225908c6de791adee8dd949f7ca4ef0f5f5
Olivia <oaaole@Olivias-MBP-2.socal.rr.com>
2018-02-24 00:16:56 -0800

    finalized

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,103 +8,88 @@ $('#start').on('click', function() {
   $(document).on('click', '#reset', function() {
     game.reset()
   })
-  // questions array for game
+  // question objects for game
 var questions = [
   {
     question: "How many continents are there in total?",
        answers: ["Four", "Nine", "Seven", "Three"],
        correctAnswer: "Seven",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "On which continent could you find France?",
        answers: ["Asia", "Africa", "Australia", "Europe"],
        correctAnswer: "Europe",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Canada is located on which continent?",
        answers: ["North America", "Antartica", "South America", "Europe"],
        correctAnswer: "North America",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent can Egypt be found on?",
        answers: ["North America", "Antartica", "South America", "Africa"],
        correctAnswer: "Africa",
-       // image: "assets/images/fsfsfwefw"
+      
    },
    {
        question: "Which continent can you find India?",
        answers: ["Europe", "Asia", "South America", "Africa"],
        correctAnswer: "Asia",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent can you find penquins in the wild?",
        answers: ["Asia", "Antartica", "Africa", "North America"],
        correctAnswer: "Antartica",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Where could you find Brazil?",
        answers: ["South America", "Africa", "Australia", "Europe"],
        correctAnswer: "South America",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Where could you find Spain?",
        answers: ["South America", "Africa", "Australia", "Europe"],
        correctAnswer: "Europe",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent is home to Iceland?",
        answers: ["Europe", "Asia", "Antartica", "South America"],
        correctAnswer: "Europe",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Where is Kenya located?",
        answers: ["Africa", "Australia", "South America", "Europe"],
        correctAnswer: "Africa",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent is the most populated in the world?",
        answers: ["Africa", "Asia", "South America", "Europe"],
        correctAnswer: "Asia",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent has the most countries?",
        answers: ["Africa", "Asia", "South America", "Europe"],
-       correctAnswer: "Africa",
-       // image: "assets/images/fsfsfwefw"    
+       correctAnswer: "Africa",   
    },
    {
        question: "Which continent is a Country in itself?",
        answers: ["Africa", "Australia", "Asia", "Antartica"],
        correctAnswer: "Australia",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent exorts the highest volume of food?",
        answers: ["Europe", "Australia", "North America", "South America"],
        correctAnswer: "North America",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent is the largest producer of coffee?",
        answers: ["Europe", "Asia", "Africa", "South America"],
        correctAnswer: "South America",
-       // image: "assets/images/fsfsfwefw"
    },
    {
        question: "Which continent is the only one in the world with no active volcanoes?",
        answers: ["Africa", "Australia", "South America", "Antartica"],
        correctAnswer: "Australia",
-       // image: "assets/images/fsfsfwefw"
    },]
   //  game array/ score keeps/ options button control
 var game = {
@@ -119,6 +104,7 @@ var game = {
     game.counter--
     $('#counter').html(game.counter)
     if(game.counter<=0) {
+      alert("OUT OF TIME! Next Question.")
       console.log("TIME UP!")
       game.timeUp()
     }
@@ -158,9 +144,9 @@ var game = {
     clearInterval(timer)
     $('#subwrapper').html('<h2>ALL DONE!</h2>')
     $('#subwrapper').append("<h3>Correct: "+game.correct+"</h3>")
-    $('#subwrapper').append("<h3>incorrect:"+game.incorrect+"</h3>")
+    $('#subwrapper').append("<h3>Incorrect: "+game.incorrect+"</h3>")
     $('#subwrapper').append("<h3>Unanswered: "+game.unanswered+"</h3>")
-    $('#subwrapper').append("<button id='reset'>RESET</button>")
+    $('#subwrapper').append("<button id='reset'>Try Again</button>")
   },
   clicked: function(e) {
     clearInterval(timer)
